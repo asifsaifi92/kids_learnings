152e4a69fc8401d518613096ba694a18b67a1d78
asifsaifi92 <asifsaifi92@gmail.com>
2026-02-01 21:44:20 +0530

    crash fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,6 +7,9 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
